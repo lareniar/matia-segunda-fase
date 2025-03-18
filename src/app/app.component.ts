7e2f88bc6909d3f115c8fd90fb75fa1bc48a811a
aritz <aritz_egiguren@hotmail.com>
2025-03-18 20:17:39 +0100

    feat: create service & component directories, create components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { FormComponent } from './components/form/form-component';
+import { HeaderComponent } from './components/header/header-component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormComponent, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'matia-segunda-fase';
